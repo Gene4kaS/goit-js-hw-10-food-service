09b744b0dce3321620a6a09a270d638e61ae12b5
Gene4kaS <jannasavchenko@gmail.com>
2021-10-11 21:11:43 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import './sass/main.scss';
+import './styles.css'
+import createMenu from './templates/menu.hbs';
+import menu from './templates/menu.json';
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const refs = {
+    menuList: document.querySelector('.js-menu'),
+    buttonToggle: document.querySelector('.theme-switch__toggle'),
+    body: document.body,
+};
+
+(function () {
+    const markup = createMenu(menu);
+    refs.menuList.insertAdjacentHTML('beforeend', markup);
+    refs.body.classList.add(localStorage.getItem('Theme') ?? Theme.LIGHT);
+    refs.buttonToggle.checked = localStorage.getItem('Theme') === Theme.DARK;
+})()
+
+// const markup = createMenu(menu);
+//     refs.menuList.insertAdjacentHTML('beforeend', markup);
+
+refs.buttonToggle.addEventListener('click', onChangeTheme);
+function onChangeTheme(e) {
+    if (e.target.checked) {
+        toggleTheme(Theme.DARK, Theme.LIGHT);
+    }
+    else {
+        toggleTheme(Theme.LIGHT, Theme.DARK);
+    }
+}
+
+function toggleTheme(add, rem) {
+    refs.body.classList.add(add);
+    refs.body.classList.remove(rem);
+    localStorage.setItem('Theme', add);
+}
